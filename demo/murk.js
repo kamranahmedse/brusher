d05fdb3e3a2c1467720ca1353ab4653a3c3a259f
Kamran Ahmed <kamranahmed.se@gmail.com>
2018-05-05 11:57:27 +0200

    Prepare drawing canvas

--- demo/murk.js
+++ demo/murk.js
@@ -1,7 +1,8 @@
 class Murk {
     constructor(options = {}) {
-        // Holds the positions where mouse was moved
-        this.tailSteps = [];
+        this.mouseSteps = [];
+        this.drawBoardCanvas = null;
+        this.drawBoardCanvasContext = null;
 
         this.options = {
             element: 'body',
@@ -19,11 +20,55 @@ class Murk {
         // @todo bindings for mouse move to draw tail
     }
 
+    /**
+     * Prepares the canvases i.e. one upon which we will draw the image
+     * and the other holds just the image for us to draw on to the
+     * drawing canvas
+     */
     prepareCanvas() {
-        // @todo create canvas upon which the non-blurred image will be drawn
+        this.prepareDrawingCanvas();
+        this.prepareColoredImageCanvas();
+    }
+
+    /**
+     * Prepares the canvas attached to the document, upon which we
+     * will draw the non-blurred image
+     */
+    prepareDrawingCanvas() {
+        if (this.drawBoardCanvas && this.drawBoardCanvas.parentElement) {
+            this.drawBoardCanvas.parentElement.removeChild(this.drawBoardCanvas);
+        }
+
+        const elementDimensions = this.getElementDimensions();
+
+        const canvas = document.createElement("canvas");
+        canvas.width = elementDimensions.width;
+        canvas.height = elementDimensions.height;
+
+        this.drawBoardCanvas = canvas;
+        this.drawBoardCanvasContext = canvas.getContext("2d");
+
+        document.body.appendChild(this.drawBoardCanvas);
+    }
+
+    prepareColoredImageCanvas() {
+
     }
 
     drawTail() {
         // @todo draw tail on the path where mouse was last moved
+    }
+
+    getElementNode() {
+        const element = this.options.element;
+        if (element && typeof element === 'object' && 'nodeType' in element) {
+            return element;
+        }
+
+        return document.querySelector(element || 'body');
+    }
+
+    getElementDimensions() {
+        return this.getElementNode().getBoundingClientRect();
     }
 }
